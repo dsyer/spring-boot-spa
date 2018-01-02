f12d597f5b8212223b773d11b8bfb79765cb35dc
Dave Syer <dsyer@pivotal.io>
2018-01-02 14:46:43 +0000

    Add some tests

--- src/test/java/com/example/demo/StaticDemoApplicationTests.java
+++ src/test/java/com/example/demo/StaticDemoApplicationTests.java
@@ -2,15 +2,34 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class StaticDemoApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void missing() throws Throwable {
+		mockMvc.perform(get("/missing")).andExpect(status().is3xxRedirection())
+				.andExpect(header().string("Location", "/"));
+	}
+
+	@Test
+	public void home() throws Throwable {
+		mockMvc.perform(get("/")).andExpect(status().isOk());
 	}
 
 }
